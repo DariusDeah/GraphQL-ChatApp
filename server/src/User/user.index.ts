0754bfc07b9c56d9a212177026f11a5f0c9c25a5
darius <dariusdeah21@gmail.com>
2022-07-05 13:13:13 -0600

    user mutation added to schema

--- server/src/User/user.index.ts
+++ server/src/User/user.index.ts
@@ -1,6 +1,8 @@
 import { GraphQLSchema } from "graphql";
+import { UserMutation } from "./user.mutation";
 import { UserRootQuery } from "./user.query";
 
 export const UserSchema = new GraphQLSchema({
   query: UserRootQuery,
+  mutation: UserMutation,
 });
