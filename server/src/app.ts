e37a5e173eedd6fd6ffec41d8b7b51e61f61f303
darius <dariusdeah21@gmail.com>
2022-07-05 13:48:15 -0600

    cookie session added

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { UserSchema } from "./User/user.index";
 import cors from "cors";
+import { COOKIE_CONFIG } from "./config";
+import cookieSession from "cookie-session";
 export const app: express.Application = express();
 
 app.use(cors());
@@ -12,5 +14,12 @@ app.use(
   graphqlHTTP({
     graphiql: true,
     schema: UserSchema,
+  })
+);
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 1000,
+    keys: [COOKIE_CONFIG.secret1, COOKIE_CONFIG.secret2],
   })
 );
