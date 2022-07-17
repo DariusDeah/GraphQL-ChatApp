95a05f0e8c8cde6a89362d82d146db9ae63131ac
DariusDeah <dariusdeah21@gmail.com>
2022-07-17 16:09:04 -0600

    find users query created

--- client/graphql/user.graphql.ts
+++ client/graphql/user.graphql.ts
@@ -1,16 +1,14 @@
 import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
-
 const client = new ApolloClient({
   uri: "localhost:5000/graphql",
   cache: new InMemoryCache(),
 });
 
-client.query({
-  query: gql`
-    {
-      users {
-        name
-      }
-    }
-  `,
-});
+export const findUsers = gql`
+query users($name:string){
+  user(name:$name){
+    id
+    name
+    email
+}
+`;
