86f20cc1e5eb46f7f9213e24967d8d2b6d7c3881
DariusDeah <dariusdeah21@gmail.com>
2022-07-17 20:40:57 -0600

    search page profile cards added

--- client/pages/Search.tsx
+++ client/pages/Search.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import { Grid } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import React from "react";
 import Nav from "../components/Nav/Nav";
 import ProfileCard from "../components/ProfileCard/ProfileCard";
@@ -10,41 +10,39 @@ type Props = {};
 
 function Search({}: Props) {
   const { data, loading, error } = useQuery(findUsers, {
-    variables: { name: "happy chatter" },
+    variables: { name: "New User guy" },
   });
   if (data) {
     console.log({ data });
   }
 
   return (
-    <Grid container>
+    <Container>
       <Grid item xs={12}>
         <Nav />
       </Grid>
 
       <Grid
-        container
         item
         xs={12}
+        marginY={5}
         style={{
           marginTop: "6rem",
-          display: "flex",
           justifyContent: "center",
         }}
       >
         <SearchBar />
       </Grid>
 
-      <Grid item xs={12} md={3} flexDirection="row">
+      <Grid container columnSpacing={2} rowSpacing={2}>
         {data &&
           data.users.map((user: any) => (
-            <ProfileCard userData={user} key={user._id} />
+            <Grid item xs={4}>
+              <ProfileCard userData={user} key={user._id} />
+            </Grid>
           ))}
-        <ProfileCard userData={{ name: "Jake", email: "jake@gmail.com" }} />
-        <ProfileCard userData={{ name: "Jake", email: "jake@gmail.com" }} />
-        <ProfileCard userData={{ name: "Jake", email: "jake@gmail.com" }} />
       </Grid>
-    </Grid>
+    </Container>
   );
 }
 
